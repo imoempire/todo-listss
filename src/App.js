0d4e8ae0e467f7a6d0317e1bc9688496bfb4fefb
imoempire <mrimoempire@gmail.com>
2024-04-14 17:21:54 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // STATE FOR THE TODOS
+  const [todolist, setTodoList] = useState([]);
+
+  // STATE FOR THE INPUT BOX
+  const [inputText, setInputText] = useState("");
+
+  // ADD TODO FUNCTION
+  const addTodo = () => {
+    if (!inputText) {
+      console.log("Please enter a text");
+      return false;
+    }
+    let newTodo = {
+      id: Date.now(),
+      name: inputText,
+    };
+
+    setTodoList([...todolist, newTodo]);
+    setInputText("");
+  };
+
+  const removeTodo = (id) => {
+    let newTodo = todolist.filter((todo) => todo.id !== id);
+    setTodoList(newTodo);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 10,
+        }}
+      >
+        <input
+          placeholder="add todo"
+          style={{
+            border: "1px solid red",
+            borderRadius: "5px",
+          }}
+          value={inputText}
+          onChange={(value) => {
+            setInputText(value.target.value);
+          }}
+        />
+        <button onClick={addTodo}>Add todo</button>
+      </div>
+      <div>
+        <p>All Todo list </p>
+        {todolist?.map((text) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: 10,
+              }}
+              key={text.id}
+            >
+              <p>{text.name}</p>
+              <span
+                onClick={() => removeTodo(text.id)}
+                style={{ color: "red" }}
+              >
+                delete
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Components/RenderTodo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RenderTodo = () => {
+  return <div>RenderTodo</div>;
+};
+
+export default RenderTodo;
